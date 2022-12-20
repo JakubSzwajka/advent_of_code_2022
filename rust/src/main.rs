570cf41787115575eea7cd0338f2c4dd4fffb97a
Jakub Szwajka <szwajkajakub@gmail.com>
2022-12-20 10:53:23 +0100

    Added day template

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,17 +3,20 @@ use anyhow::Result;
 mod common;
 mod day11;
 mod day12;
+mod day13;
 mod solution;
 
 use crate::solution::Solution;
 
 use day11::{Day11Pt1, Day11Pt2};
 use day12::{Day12Pt1, Day12Pt2};
+use day13::Day13Pt1;
 
 fn main() -> Result<()> {
     Day11Pt1::run()?;
     Day11Pt2::run()?;
     Day12Pt1::run()?;
     Day12Pt2::run()?;
+    Day13Pt1::run()?;
     Ok(())
 }
